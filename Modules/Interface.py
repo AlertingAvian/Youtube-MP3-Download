1e92d81f06f593d4eb511ab99536f979a3a3f742
Patrick <boyscoutpatrick813@gmail.com>
2020-04-06 01:49:27 -0500

    WIP

---
+++ Modules/Interface.py
@@ -0,0 +1,29 @@
+import PySimpleGUI as sg
+from time import sleep
+
+sg.change_look_and_feel('DefaultNoMoreNagging')
+
+frame1_layout = [
+                [sg.Image(filename='image.png',size=(None,None))]
+                ]
+
+layout = [
+            [sg.Input(),sg.Submit()],
+            [sg.Frame('Results',frame1_layout,'red')],
+            [sg.Exit()]
+            ]
+
+window = sg.Window('Window',layout)
+
+while True:
+    event, values = window.read()
+
+    # print(event, values) # debug
+
+    if event in (None, 'Exit', 'Cancel'):
+        break
+
+    elif event[:4:] == 'Exit':
+        break
+    
+window.close()
---
+++ Modules/youtube_search.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+import lxml
+
+def search_youtube(search_term):
+    # format search term into youtube search url
+    formatted_search_term = ''
+    search_url = 'https://www.youtube.com/results?search_query='
+
+    for char in search_term:
+        if char.isspace():
+            formatted_search_term += ('+')
+        else:
+            formatted_search_term += (char)
+
+    search_url += formatted_search_term
+
+    # retrive video thumbnail, url, and title
+    text = requests.get(search_url).text
+    soup = BeautifulSoup(text,features="lxml")
+    divs = [div for div in soup.find_all('div') if div.has_attr('class') and 'yt-lockup-dismissable' in div['class']]
+    thumbnail_src = divs[0].find_all('img')[0]['src']
+    link_lst = [x for x in divs[0].find_all('a') if x.has_attr('title')]
+    video_url = 'https://youtu.be/' + link_lst[0]['href'][9::]
+    video_title = link_lst[0]['title']
+    return(thumbnail_src,video_url,video_title)
