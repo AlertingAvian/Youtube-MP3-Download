9905ea74aeedb687515c12128a70d64ae54a604c
Patrick <boyscoutpatrick813@gmail.com>
2020-04-07 02:30:50 -0500

    Save Manager Update

--- Modules/save_manager.py
+++ Modules/save_manager.py
@@ -7,30 +7,29 @@ class save_manager(object):
         self.cwd = pathlib.Path.cwd()
         self.sp = self.cwd.joinpath(pathlib.Path('YTMD_SaveData'))
         self.isp = self.sp.joinpath(pathlib.Path('YTMD_ImageSaveData'))
+        self.lsp = self.sp.joinpath(pathlib.Path('YTMD_Logs'))
+        self.msp = self.sp.joinpath(pathlib.Path('YTMD_MP3'))
+        self.paths = [self.sp, self.isp, self.lsp, self.msp]
+        self.file_type_save_loc = {'img':self.isp,}
         print('[SUCCESS] Save Manager Initialized.')
     
     def prepare_save(self):
         print('[INFO] Checking Save Directories')
-        try:
-            if not self.sp.is_dir():
-                print(f'[NOTICE] \'{self.sp}\' missing. Creating...')
-                self.sp.mkdir()
-                print(f'[SUCCESS] \'{self.sp}\' Created.')
-            if not self.isp.is_dir():
-                print(f'[NOTICE] \'{self.isp}\' missing. Creating...')
-                self.isp.mkdir()
-                print(f'[SUCCESS] \'{self.sp}\' Created.')
-        except Exception as e:
-            print('[ERROR] Save Directory Error')
-            return e
-        else:
-            print('[SUCCESS] Save Directory Complete.')
-            return True
-    
+        for path in self.paths:
+            if not path.is_dir():
+                print(f'[INFO] \'{path}\' missing. Creating...''')
+                try:
+                    path.mkdir()
+                except Exception as e:
+                    print('[ERROR] Unabe to create \'{path}\'')
+                    return e
+                else:
+                    print(f'[SUCCESS] \'{path}\' Created.')
+                    return True
 
+    def save_img(self, img_url):
+        pass
 
-def save():
-    pass
-
-def rm_save():
-    pass
+    def save_log(self):
+        pass
+        
